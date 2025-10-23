5692434f131055384c307fe5aaef860b9851aced
LazyPhilo <118143675+EthanOwO1@users.noreply.github.com>
2025-10-23 13:43:47 -0400

    Create HelloWorld.java
    created first ever java class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Ethan"); // My First Comment
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    // Single Line Comment
+    /*
+    1. Hello: World, change the telemetry to "Hello: Your Name"
+    2. Run this code in the Autonomous section of your DS
+
+    */
+}
